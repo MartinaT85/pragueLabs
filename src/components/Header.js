c5250be5a5101a7b4a198ac2e6990ae599b9f19f
Martina Tavoda <martinat@tutanota.com>
2022-07-05 20:05:04 +0200

    logo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import NextImage from "next/image";
 
 const Header = () => {
-  return <NextImage src="/pragueLabsLogo" height={35} width={201} />;
+  return (
+    <>
+      <NextImage src="/pragueLabsLogo.svg" height={35} width={201} />
+      <h1>Hello</h1>
+    </>
+  );
 };
 
 export default Header;
