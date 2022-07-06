1bd13da1799ff0571026afa26d9ecf2c51403a12
Martina Tavoda <martinat@tutanota.com>
2022-07-06 19:59:28 +0200

    header basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import NextImage from "next/image";
+import styled from "styled-components";
 
 const Header = () => {
   return (
     <>
-      <NextImage src="/pragueLabsLogo.svg" height={35} width={201} />
-      <h1>Hello</h1>
+      <div className="wrapper">
+        <LogoContainer className="logo-container">
+          <NextImage src="/pragueLabsLogo.svg" height={35} width={201} />
+        </LogoContainer>
+      </div>
     </>
   );
 };
+
+const LogoContainer = styled.div`
+  margin: 1rem 0;
+`;
 
 export default Header;
